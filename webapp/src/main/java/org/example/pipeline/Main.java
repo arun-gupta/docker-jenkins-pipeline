d0ecbfa7d791865ff417cb8c2dbc5e70620123dd
Arun Gupta <arun.gupta@gmail.com>
2016-08-26 12:38:53 -0700

    beginning work with manual commands for pipeline

--- webapp/src/main/java/org/example/pipeline/Main.java
+++ webapp/src/main/java/org/example/pipeline/Main.java
@@ -2,12 +2,9 @@ package org.example.pipeline;
 
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
+public class Main {
+    public static void main( String[] args ) {
         System.out.println( "Hello World!" );
     }
 }
